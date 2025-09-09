637f78015c026ffc9e67daf60cb4515d942ac231
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-09-09 12:26:00 +0200

    Update test_r_runner.py

--- tests/test_r_runner.py
+++ tests/test_r_runner.py
@@ -30,7 +30,7 @@ def test_rxp_make_real_pipeline_runs():
 
     result = rxp_make(
         script=str(repo_gen),
-        verbose=0,
+        verbose=1,
         max_jobs=1,
         cores=1,
         timeout=300,
