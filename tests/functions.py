9fc34eb0f0263d0e7c1238664f13b67298d24b7c
Bruno Rodrigues <bruno@brodrigues.co>
2026-01-22 15:43:22 +0100

    Deployed 6917465 with MkDocs version: 1.6.1

--- tests/functions.py
+++
@@ -1,3 +0,0 @@
-def serialize_to_json(pl_df, path):
-    with open(path, 'w') as f:
-        f.write(pl_df.write_json())
--- tests/test_hello.py
+++
@@ -1,7 +0,0 @@
-from ryxpress import hello, __version__
-
-def test_hello():
-    assert hello() == "Hello from ryxpress!"
-
-def test_version():
-    assert isinstance(__version__, str)
